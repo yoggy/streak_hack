8d0186438ff883968e65ac226a5ab05f877d25e4
yoggy <yoggy0@gmail.com>
2013-07-24 19:56:07 +0900

    1374663367

--- streak_hack.sh
+++ streak_hack.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-
+#
+#  streak_hack.sh - a joke shell script
+#
 EPOCH_FILE=epoch.txt
 
 cd $(dirname $0)
 rm -rf ${EPOCH_FILE}
 date +%s > ${EPOCH_FILE}
 git commit -a -m `date +%s`
-
+git push -u origin master
